1737ff59c0750d7ad385f08efcb9a726536681c2
Frederick Mannings <fred.mannings@solvicode.com>
2025-12-30 22:31:53 +0000

    Added python type stub exports back in to the cli

---
+++ stub/main.go
@@ -0,0 +1,291 @@
+package stub
+
+import (
+	"embed"
+	"fmt"
+	"hash/crc32"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"text/template"
+
+	pb "github.com/orc-analytics/core/protobufs/go"
+)
+
+const PYTHON_STUB_FILE = "stub_templates/processor.pyi.tmpl"
+
+//go:embed stub_templates/*.tmpl
+var templateFS embed.FS
+
+var pythonTemplate *template.Template
+
+type ReturnType string
+
+const (
+	structReturnType ReturnType = "StructResult"
+	valueReturnType  ReturnType = "ValueResult"
+	noneReturnType   ReturnType = "NoneResult"
+	arrayReturnType  ReturnType = "ArrayResult"
+)
+
+func init() {
+	baseName := filepath.Base(PYTHON_STUB_FILE)
+	pythonTemplate = template.Must(template.New(baseName).Funcs(
+		template.FuncMap{
+			"ToSnakeCase":          toSnakeCase,
+			"SanitiseVariableName": sanitiseVariableName,
+			"WrapText":             wrapText,
+			"Indent":               pythonIndent,
+		}).ParseFS(templateFS, PYTHON_STUB_FILE))
+}
+
+func wrapText(limit int, text string) string {
+	words := strings.Fields(strings.TrimSpace(text))
+	if len(words) == 0 {
+		return ""
+	}
+
+	var result strings.Builder
+	currentLineLength := 0
+
+	for i, word := range words {
+		// If adding the word + a space exceeds the limit
+		if currentLineLength+len(word) > limit && currentLineLength > 0 {
+			result.WriteString("\n")
+			currentLineLength = 0
+		} else if i > 0 {
+			result.WriteString(" ")
+			currentLineLength++
+		}
+
+		result.WriteString(word)
+		currentLineLength += len(word)
+	}
+
+	return result.String()
+}
+
+// Helper to indent lines for Python docstrings
+func pythonIndent(spaces int, text string) string {
+	prefix := strings.Repeat(" ", spaces)
+	return prefix + strings.ReplaceAll(text, "\n", "\n"+prefix)
+}
+
+func toSnakeCase(s string) string {
+	var result []rune
+	for i, r := range s {
+		if i > 0 && r >= 'A' && r <= 'Z' {
+			result = append(result, '_')
+		}
+		if r >= 'A' && r <= 'Z' {
+			result = append(result, r+32)
+		} else {
+			result = append(result, r)
+		}
+	}
+	return string(result)
+}
+
+func sanitiseVariableName(s string) string {
+	var result []rune
+	for i, r := range s {
+		if i == 0 {
+			if _, err := strconv.Atoi(string(r)); err == nil {
+				result = append(result, '_')
+				result = append(result, r)
+				continue
+			}
+		}
+		if r == '.' {
+			result = append(result, '_')
+		} else {
+			result = append(result, r)
+		}
+
+	}
+	return string(result)
+}
+
+// data structures matching the template expectations
+type Metadata struct {
+	VarName     string
+	KeyName     string
+	Description string
+}
+
+type Window struct {
+	VarName          string
+	Name             string
+	Version          string
+	Description      string
+	MetadataVarNames []string
+}
+
+type Algorithm struct {
+	Name          string
+	VarName       string
+	Description   string
+	ProcessorName string
+	Version       string
+	WindowVarName string
+	ReturnType    ReturnType
+	Hash          string
+}
+
+type ProcessorData struct {
+	Name       string
+	Metadata   []Metadata
+	Windows    []Window
+	Algorithms []Algorithm
+}
+
+type AllProcessors struct {
+	Processors  []ProcessorData
+	ImportTypes []string
+	AllMetadata []Metadata
+	AllWindows  []Window
+}
+
+func mapInternalStateToTmpl(internalState *pb.InternalState) (error, *AllProcessors) {
+	processorDatas := make([]ProcessorData, len(internalState.GetProcessors()))
+
+	usedReturnTypes := make(map[string]bool)
+	globalMetadataMap := make(map[string]Metadata)
+	globalWindowsMap := make(map[string]Window)
+
+	for ii, proc := range internalState.GetProcessors() {
+		supportedAlgorithms := make([]Algorithm, len(proc.GetSupportedAlgorithms()))
+
+		for jj, algo := range proc.GetSupportedAlgorithms() {
+			windowName := algo.GetWindowType().GetName()
+			windowVer := algo.GetWindowType().GetVersion()
+			windowKey := fmt.Sprintf("%v_%v", windowName, windowVer)
+			windowVarName := fmt.Sprintf("%v_stub", windowKey)
+
+			metadataVarNamesForWindow := make([]string, len(algo.GetWindowType().GetMetadataFields()))
+			for kk, metadata := range algo.GetWindowType().GetMetadataFields() {
+				mName := metadata.GetName()
+				mVarName := fmt.Sprintf("%v_stub", mName)
+
+				if _, ok := globalMetadataMap[mVarName]; !ok {
+					globalMetadataMap[mVarName] = Metadata{
+						VarName:     mVarName,
+						KeyName:     mName,
+						Description: metadata.GetDescription(),
+					}
+				}
+				metadataVarNamesForWindow[kk] = mVarName
+			}
+
+			if _, ok := globalWindowsMap[windowVarName]; !ok {
+				globalWindowsMap[windowVarName] = Window{
+					VarName:          windowVarName,
+					Name:             windowName,
+					Version:          windowVer,
+					Description:      algo.GetWindowType().GetDescription(),
+					MetadataVarNames: metadataVarNamesForWindow,
+				}
+			}
+
+			var algoReturnType ReturnType
+			switch algo.GetResultType() {
+			case pb.ResultType_ARRAY:
+				algoReturnType = arrayReturnType
+			case pb.ResultType_STRUCT:
+				algoReturnType = structReturnType
+			case pb.ResultType_VALUE:
+				algoReturnType = valueReturnType
+			case pb.ResultType_NONE:
+				algoReturnType = noneReturnType
+			case pb.ResultType_NOT_SPECIFIED:
+				return fmt.Errorf(
+					"result type not specified for algorithm %v_%v on processor %v_%v",
+					algo.GetName(),
+					algo.GetVersion(),
+					proc.GetName(),
+					proc.GetRuntime(),
+				), nil
+			}
+			usedReturnTypes[string(algoReturnType)] = true
+
+			h := crc32.NewIEEE()
+			h.Write([]byte(proc.GetName()))
+			h.Write([]byte(proc.GetConnectionStr()))
+			h.Write([]byte(windowName))
+			h.Write([]byte(windowVer))
+			h.Write([]byte(algo.GetName()))
+			h.Write([]byte(algo.GetVersion()))
+			algorithmHash := h.Sum32()
+
+			supportedAlgorithms[jj] = Algorithm{
+				Name:          algo.GetName(),
+				VarName:       fmt.Sprintf("%v_%x", algo.GetName(), algorithmHash),
+				ProcessorName: proc.GetName(),
+				Version:       algo.GetVersion(),
+				ReturnType:    algoReturnType,
+				WindowVarName: windowVarName,
+				Hash:          fmt.Sprintf("%x", algorithmHash),
+				Description:   algo.GetDescription(),
+			}
+		}
+
+		processorDatas[ii] = ProcessorData{
+			Name:       proc.GetName(),
+			Algorithms: supportedAlgorithms,
+		}
+	}
+
+	// Convert Global Metadata Map to Slice
+	allMetadata := make([]Metadata, 0, len(globalMetadataMap))
+	for _, m := range globalMetadataMap {
+		allMetadata = append(allMetadata, m)
+	}
+
+	// Convert Global Windows Map to Slice
+	allWindows := make([]Window, 0, len(globalWindowsMap))
+	for _, w := range globalWindowsMap {
+		allWindows = append(allWindows, w)
+	}
+
+	// Finalize Import List
+	importList := []string{}
+	availableTypes := []string{"StructResult", "ValueResult", "NoneResult", "ArrayResult"}
+	for _, t := range availableTypes {
+		if usedReturnTypes[t] {
+			importList = append(importList, t)
+		}
+	}
+
+	return nil, &AllProcessors{
+		Processors:  processorDatas,
+		ImportTypes: importList,
+		AllMetadata: allMetadata,
+		AllWindows:  allWindows,
+	}
+}
+
+func GeneratePythonStub(internalState *pb.InternalState, outDir string) error {
+
+	err, processorTmpl := mapInternalStateToTmpl(internalState)
+	if err != nil {
+		return fmt.Errorf("could not parse internal state: %w", err)
+	}
+
+	err = os.Mkdir(outDir, 0750)
+
+	if err != nil && !os.IsExist(err) {
+		return (err)
+	}
+
+	outFile, err := os.Create(filepath.Join(outDir, "orca_stub.pyi"))
+	if err != nil && !os.IsExist(err) {
+		return err
+	}
+
+	defer outFile.Close()
+	if err := pythonTemplate.Execute(outFile, processorTmpl); err != nil {
+		panic(err)
+	}
+	return nil
+}
